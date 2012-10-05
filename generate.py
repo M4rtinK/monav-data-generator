49344db90590d3943b6b603956903ff0d537e966
Martin Kolman <martin.kolman@gmail.com>
2012-10-06 00:07:19 +0200

    don't generate address tries - looks like they are not actually needed to show route names in routing results

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ import os
 import zipfile
 import shutil
 
-THREADS=4
+THREADS = 4
 
 if len(sys.argv) < 2:
   print('Error: not enough arguments')
@@ -36,16 +36,19 @@ def zipdir(path, zipFilename):
 # create car, bike and foot routing data
 
 # first pass - import data, create address info & generate car routing data
-args1 = ['monav-preprocessor', '-di', '-dro="car"', '-da=unicode_tournament_trie', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"', '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="motorcar"']
+args1 = ['monav-preprocessor', '-di', '-dro="car"', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"',
+         '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="motorcar"']
 # second pass - generate bike routing data
-args2 = ['monav-preprocessor', '-dro="bike"', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"', '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="bicycle"']
+args2 = ['monav-preprocessor', '-dro="bike"', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"',
+         '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="bicycle"']
 # third pass - process pedestrian routing data & delete temporary files
-args3 = ['monav-preprocessor', '-dro="pedestrian"', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"', '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="foot"', '-dd']
+args3 = ['monav-preprocessor', '-dro="pedestrian"', '-t=%d' % THREADS, '--verbose', '--settings="base.ini"',
+         '--input="%s"' % inputFile, '--output="%s"' % outputFolder, '--name="%s"' % name, '--profile="foot"', '-dd']
 
 # convert the arguments to whitespace delimited strings and run them
-os.system(reduce(lambda x, y: x+" "+y, args1))
-os.system(reduce(lambda x, y: x+" "+y, args2))
-os.system(reduce(lambda x, y: x+" "+y, args3))
+os.system(reduce(lambda x, y: x + " " + y, args1))
+os.system(reduce(lambda x, y: x + " " + y, args2))
+os.system(reduce(lambda x, y: x + " " + y, args3))
 print('data processing done')
 
 ## compress the results
